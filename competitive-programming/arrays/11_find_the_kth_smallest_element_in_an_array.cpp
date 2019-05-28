f662ed8eae3ff10d92dffb1fd40a172547487ed4
augustine <augustinetharakan12@gmail.com>
2019-05-28 13:15:47 +0530

    kth smallest element bug fix

--- competitive-programming/arrays/11_find_the_kth_smallest_element_in_an_array.cpp
+++ competitive-programming/arrays/11_find_the_kth_smallest_element_in_an_array.cpp
@@ -16,27 +16,21 @@ int main() {
 		cin >> arr[i];
 
 	for(i=0; i<k ;i++) {
-		for(j=i+1; j<k; j++)
-			if(arr[j]<arr[i]) {
-				temp = arr[i];
-				arr[i] = arr[j];
-				arr[j] = temp;
-
-				arr2[i] = arr[i];
-				arr2[j] = arr[j];
-			}
+		arr2[i] = arr[i];
 	}
+
+	sort(arr2, arr2+k);
 
 	for(i=k; i<limit; i++) {
 		j = k-1;
 		flag = 0;
 		while(arr2[j] > arr[i] && j>=0) {
 			arr2[j+1] = arr2[j];
-			j--;
 			flag = 1;
+			j--;
 		}
 		if(flag == 1)
-			arr2[j] = arr[i];
+			arr2[j+1] = arr[i];
 	}
 
 	cout << arr2[k-1];
